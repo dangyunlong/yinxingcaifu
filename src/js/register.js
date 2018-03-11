36e6c9f0752370d1f0532d5a54f87f5e67149af3
Sean <381332123@qq.com>
2018-03-11 09:43:55 +0800

    提交表单搞定了
    提交表单搞定了

--- src/js/register.js
+++ src/js/register.js
@@ -24,20 +24,37 @@ define(function (require) {
 		//这真是我见过的最扯淡的写法      自定义规则 手机
 		validate.Validator.extend('mobile', {
 		  	getMessage: field =>  field + '必须是11位手机号', //这个都不需要
-		  	validate: value => value.length == 11
+		  	validate: value => value.length == 11 && /^((13|14|15|17|18)[0-9]{1}\d{8})$/.test(value)
 		});
 		
-
+		
 	    new Vue({
 	        el: '#artbox',
+	        methods: {  
+                submit: function() {  
+	                //验证所有表单并且提交
+	                this.$validator.validateAll().then((result) => {
+				        if (result) {
+				          // eslint-disable-next-line
+				          alert('Form Submitted!');
+				          return;
+				        }
+				        alert('Correct them errors!');
+				    });
+                }  
+            },
+	        mounted(){
+	        	
+	        	//如果你的元素在控制器里面的话，你就需要写在这里面了
+	        	$("#yqm").on("click",function(){
+					$(this).toggleClass("select");
+					$("#yqmbox").toggle();
+				})
+	        }
 	    })
 	    
 	});
 	
-	$("#yqm").on("click",function(){
-		$(this).toggleClass("select");
-		$("#yqmbox").toggle();
-	})
 	
 });
 
